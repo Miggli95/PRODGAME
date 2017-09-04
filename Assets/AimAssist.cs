b67c125e77761121b20e1e10199ceb0b4d98e81b
Miggli95 <miggli@hotmail.se>
2017-09-04 12:50:40 +0200

    Man kan nu skjuta iväg spelaren

---
+++ Assets/AimAssist.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAssist : MonoBehaviour {
+    public float Angle;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(this.transform.position);//kanske vara parent transform
+        mousePos.x = mousePos.x - playerPos.x;
+        mousePos.y = mousePos.y - playerPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        Angle = angle;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+    }
+}
---
+++ Assets/ShootScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public float force;
+    public float currentForce;
+    public float min;
+    public float max;
+    public float currentPower;
+    int increase = 1;
+    public float multiplier = 1;
+    bool mouseButtonPressed = false;
+    public int increaseMultiplier = 1;
+    public float gravityMultiplier = 1;
+	// Use this for initialization
+	void Start () {
+       // Mathf.Clamp(force, min, max);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (force >= max)
+        {
+            increase = -1;
+        }
+
+        if (force <= min)
+        {
+            increase = 1;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            force += increase * Time.deltaTime * increaseMultiplier;
+            mouseButtonPressed = true;
+        }
+
+        else if (mouseButtonPressed)
+        {
+            currentForce = force;
+            force = 0;
+            mouseButtonPressed = false;
+            Shoot();
+        }
+
+        Gravity();
+    }
+
+    public void Gravity()
+    {
+        transform.GetComponent<Rigidbody>().AddForce(Physics.gravity * gravityMultiplier);
+    }
+
+    public void Shoot()
+    {
+        float angle = transform.GetComponentInChildren<AimAssist>().Angle;
+        Vector3 dir = Quaternion.AngleAxis(angle, transform.forward) * transform.right;
+        transform.GetComponent<Rigidbody>().AddForce(dir * currentForce * multiplier);
+    }
+}
