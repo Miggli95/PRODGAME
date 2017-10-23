fcd77842a62261a6ecbe6cdacfaee2d2f0636ddf
sync93 <timpaboi@live.se>
2017-10-23 18:01:05 +0100

    menu fixed

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
+
+ 
+
 {
+
+
+    public GameObject instructionOBJ;
 
     public void toGame()
     {
 
         SceneManager.LoadScene("Level1");
     }
+
+    public void toBlind()
+    {
+
+        SceneManager.LoadScene("BlindLevel");
+    }
+
+    public void toLow()
+    {
+
+        SceneManager.LoadScene("Level1Eye");
+    }
+
+    public void toIns()
+    {
+        instructionOBJ.SetActive(true);
+       
+    }
+
+
+
+
 
 }
